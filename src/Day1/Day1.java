d2ddea8a1c150cc2739165ec01d5b17ce3579c77
mrwillans <willansm@science.regn.net>
2020-12-09 09:14:53 +0000

    day9 part 1

--- src/Day1/Day1.java
+++ src/Day1/Day1.java
@@ -7,43 +7,43 @@ import java.util.HashMap;
 
 public class Day1 {
 
-    private int[] findInts(int target, int numOfSolns, ArrayList<Integer> array) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+    public Long[] findInts(Long target, int numOfSolns, ArrayList<Long> array) {
+        HashMap<Long, Integer> map = new HashMap<>();
         for (int i = 0; i < array.size(); i++) {
-            int num = array.get(i);
+            Long num = array.get(i);
             if (numOfSolns == 2) {
-                int complement = target - num;
+                Long complement = target - num;
                 if (map.containsKey(complement)) {
-                    return new int[]{complement, num};
+                    return new Long[]{complement, num};
                 }
             } else if (numOfSolns == 3) {
-                int secondTarget = target - num;
-                for (int value2 : array) {
-                    int secondComplement = secondTarget - value2;
+                Long secondTarget = target - num;
+                for (Long value2 : array) {
+                    Long secondComplement = secondTarget - value2;
                     if (map.containsKey(value2) && map.containsKey(secondComplement)) {
-                        return new int[]{value2, secondComplement, num};
+                        return new Long[]{value2, secondComplement, num};
                     }
                 }
 
             }
             map.put(num, i);
         }
-        return new int[]{-1, -1};
+        return new Long[]{-1L, -1L};
     }
 
     public static void main(String[] args) {
         try {
             BufferedReader br = new BufferedReader(new FileReader("src/Day1/input.txt"));
             String line;
-            ArrayList<Integer> array = new ArrayList<>();
+            ArrayList<Long> array = new ArrayList<>();
             while ((line = br.readLine()) != null) {
-                array.add(Integer.parseInt(line));
+                array.add(Long.parseLong(line));
             }
             Day1 day1 = new Day1();
-            int[] pair = day1.findInts(2020, 2, array);
+            Long[] pair = day1.findInts(2020L, 2, array);
             System.out.println(pair[0] * pair[1]);
-            int[] trio = day1.findInts(2020, 3, array);
-            int soln = trio[0] * trio[1] * trio[2];
+            Long[] trio = day1.findInts(2020L, 3, array);
+            Long soln = trio[0] * trio[1] * trio[2];
             System.out.println(soln);
         } catch (Exception e) {
             System.out.println(e);
--- src/Day9/Day9.java
+++ src/Day9/Day9.java
@@ -1,4 +1,45 @@
 package Day9;
 
+import Day1.Day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Day9 {
+
+    Long getFirstNum(ArrayList<Long> nums, Day1 day1) {
+        for (int i = 0, j = 25; j < nums.size(); i++, j++) {
+            Long target = nums.get(j + 1);
+
+            nums.subList(i, j);
+
+            Long[] answer = day1.findInts(target, 2, nums);
+
+            if (answer[0] == -1 && answer[1] == -1) {
+                return target;
+            }
+        }
+        return -1L;
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/Day9/input.txt"));
+            String line;
+            ArrayList<Long> array = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                array.add(Long.parseLong(line));
+            }
+
+            Day1 day1 = new Day1();
+            Day9 day9 = new Day9();
+
+            System.out.println(day9.getFirstNum(array, day1));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
