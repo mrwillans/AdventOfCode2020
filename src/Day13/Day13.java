2b2b2fcb68b2bef99391b2b919a2ea2a03a67707
mrwillans <matthew.willans@sky.com>
2020-12-14 08:20:43 +0000

    commit

--- src/Day13/Day13.java
+++ src/Day13/Day13.java
@@ -4,25 +4,51 @@ import Day12.Day12;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Day13 {
 
-    int getTime(ArrayList<String> times){
+    int getTime(ArrayList<String> times) {
         int departure = Integer.parseInt(times.get(0));
         int lowestRemainder = 1000000000;
-        String[] busses = times.get(1).replace("x,","").split(",");
-        HashMap<Integer, Integer> busAndRemainder= new HashMap<>();
+        String[] busses = times.get(1).replace("x,", "").split(",");
+        HashMap<Integer, Integer> busAndRemainder = new HashMap<>();
 
-        for(String bus : busses){
+        for (String bus : busses) {
             int remainder = Integer.parseInt(bus) - (departure % Integer.parseInt(bus));
-            if(remainder < lowestRemainder){
+            if (remainder < lowestRemainder) {
                 lowestRemainder = remainder;
             }
             busAndRemainder.put(remainder, Integer.parseInt(bus));
         }
         return busAndRemainder.get(lowestRemainder) * lowestRemainder;
+    }
+
+    Long getTimePart2(String busses) {
+        LinkedHashMap<Integer, Integer> busAndRemainder = new LinkedHashMap<>();
+        int i = 0;
+        for (String bus : busses.split(",")) {
+            if (!bus.equals("x")) {
+                busAndRemainder.put(Integer.parseInt(bus), i);
+            }
+            i++;
+        }
+     
+        doTheStuff(busAndRemainder)
+        return -1L;
+    }
+
+    int doTheStuff(int bus1, int bus2, int t0, int interval) {
+        while (true) {
+            int time1 = t0;
+            int time2 = time1 + bus2;
+
+            if (time1 - time2 == interval) {
+                return time1;
+            }
+            time1 += bus1;
+            time2 += bus2;
+        }
     }
 
     public static void main(String[] args) {
@@ -38,6 +64,7 @@ public class Day13 {
         }
         Day13 day13 = new Day13();
         System.out.println(day13.getTime(timetable));
+        System.out.println(day13.getTimePart2(timetable.get(1)));
     }
 
 }
