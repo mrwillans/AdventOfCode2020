1f0731f16410befc1084ead33a3cc0c51929a5f6
mrwillans <willansm@science.regn.net>
2020-12-15 11:52:17 +0000

    refactor for day 15 part 2

--- src/Day15/Day15.java
+++ src/Day15/Day15.java
@@ -4,44 +4,39 @@ import java.util.*;
 
 public class Day15 {
 
-    private void play(int prevNumber, HashMap<Integer, Integer> map, int j) {
-        if (j == 2021) {
-            return;
-        }
-        if (Collections.frequency(new ArrayList<Integer>(map.values()), prevNumber) > 1) {
-            int diff = j - 1 - Collections.max(getKeyByValue(map, prevNumber));
-            map.put(j, diff);
-            play(diff, map, j + 1);
-        } else {
-            map.put(j, 0);
-            play(0, map, j + 1);
-        }
-    }
-
-    private static Set<Integer> getKeyByValue(HashMap<Integer, Integer> map, int value) {
-        Set<Integer> keys = new HashSet<Integer>();
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (Objects.equals(value, entry.getValue())) {
-                keys.add(entry.getKey());
+    private void play(Long prevNumber, HashMap<Long, ArrayList<Long>> map, Long start) {
+        for (Long j = start; j < 30000001; j++) {
+            if (map.get(prevNumber).size() > 1) {
+                List<Long> l = map.get(prevNumber);
+                Long diff = l.get(l.size() - 1) - l.get(l.size() - 2);
+                if (map.containsKey(diff)) {
+                    map.get(diff).add(j);
+                } else {
+                    map.put(diff, new ArrayList<>());
+                    map.get(diff).add(j);
+                }
+                prevNumber = diff;
+            } else {
+                map.get(0L).add(j);
+                prevNumber = 0L;
             }
         }
-        keys.remove(Collections.max(keys));
-        return keys;
+        System.out.println(prevNumber);
     }
+
 
     public static void main(String[] args) {
 
-        int[] start = new int[]{0, 8, 15, 2, 12, 1, 4}; //4};
-        //int[] start = new int[]{0, 3, 6};
+        Long[] start = new Long[]{0L, 8L, 15L, 2L, 12L, 1L, 4L}; //4};
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Long, ArrayList<Long>> map = new HashMap<>();
 
         for (int i = 0; i < start.length; i++) {
-            map.put(i + 1, start[i]);
+            map.put(start[i], new ArrayList<>());
+            map.get(start[i]).add((long) i + 1);
         }
 
         Day15 day15 = new Day15();
-        day15.play(4, map, start.length + 1);
-        System.out.println(map.get(2020));
+        day15.play(4L, map, start.length + 1L);
     }
 }
