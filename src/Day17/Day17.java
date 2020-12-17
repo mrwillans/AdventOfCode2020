6c0f4a12a1916a75ac9db86c783e5c1908ee4a13
mrwillans <willansm@science.regn.net>
2020-12-17 14:29:53 +0000

    day17 p2

--- src/Day17/Day17.java
+++ src/Day17/Day17.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Day17 {
 
-    public void part1(ArrayList<String> input) {
+    private void part1(ArrayList<String> input) {
         char[][][] grid;
         grid = new char[input.size() + 20][input.size() + 20][input.size() + 20];
         for (int z = 0; z < grid.length; z++) {
@@ -64,7 +64,73 @@ public class Day17 {
 
     }
 
-    private static int checkOccupied(char[][][] seats, int i, int j, int k) {
+    public void part2(ArrayList<String> input) {
+        char[][][][] grid;
+        grid = new char[input.size() + 20][input.size() + 20][input.size() + 20][input.size() + 20];
+        for (int w = 0; w < grid.length; w++) {
+            for (int z = 0; z < grid.length; z++) {
+                for (int y = 0; y < grid.length; y++) {
+                    for (int x = 0; x < grid.length; x++) {
+                        grid[w][z][y][x] = '.';
+                    }
+                }
+            }
+        }
+
+        for (int z = 0; z < grid.length; z++) {
+            for (int y = 0; y < input.size(); y++) {
+                for (int x = 0; x < input.get(0).length(); x++) {
+                    char c = input.get(y).charAt(x);
+                    grid[grid.length / 2 - input.size() / 2][grid.length / 2 - input.size() / 2][grid.length / 2 - input.size() / 2 + y][grid.length / 2 + x] = c;
+                }
+            }
+        }
+
+        for (int cycle = 1; cycle <= 6; cycle++) {
+            char[][][][] copy = new char[grid.length][grid.length][grid.length][grid.length];
+            for (int w = 0; w < grid.length; w++) {
+                for (int z = 0; z < grid.length; z++) {
+                    for (int y = 0; y < grid.length; y++) {
+                        for (int x = 0; x < grid.length; x++) {
+                            int activeNeighbors = checkOccupied2(grid, w, z, y, x);
+                            if (grid[w][z][y][x] == '#') {
+                                if (activeNeighbors == 2 || activeNeighbors == 3) {
+                                    copy[w][z][y][x] = '#';
+                                } else {
+                                    copy[w][z][y][x] = '.';
+                                }
+                            } else if (grid[w][z][y][x] == '.') {
+                                if (activeNeighbors == 3) {
+                                    copy[w][z][y][x] = '#';
+                                } else {
+                                    copy[w][z][y][x] = '.';
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            grid = copy;
+        }
+
+        int active = 0;
+        for (int w = 0; w < grid.length; w++) {
+            for (int z = 0; z < grid.length; z++) {
+                for (int y = 0; y < grid.length; y++) {
+                    for (int x = 0; x < grid.length; x++) {
+                        if (grid[w][z][y][x] == '#') {
+                            active++;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("Cubes: " + active);
+    }
+
+    private int checkOccupied(char[][][] seats, int i, int j, int k) {
         int count = 0;
         for (int dx = (i > 0 ? -1 : 0); dx < (i < seats.length - 1 ? 2 : 1); dx++) {
             for (int dy = (j > 0 ? -1 : 0); dy < (j < seats.length - 1 ? 2 : 1); dy++) {
@@ -80,6 +146,25 @@ public class Day17 {
         return count;
     }
 
+    private int checkOccupied2(char[][][][] seats, int i, int j, int k, int l) {
+        int count = 0;
+        for (int dx = (i > 0 ? -1 : 0); dx < (i < seats.length - 1 ? 2 : 1); dx++) {
+            for (int dy = (j > 0 ? -1 : 0); dy < (j < seats.length - 1 ? 2 : 1); dy++) {
+                for (int dz = (k > 0 ? -1 : 0); dz < (k < seats.length - 1 ? 2 : 1); dz++) {
+                    for (int dw = (l > 0 ? -1 : 0); dw < (l < seats.length - 1 ? 2 : 1); dw++) {
+                        if (dx != 0 || dy != 0 || dz != 0 || dw != 0) {
+                            if (seats[i + dx][j + dy][k + dz][l + dw] == '#') {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
         ArrayList<String> input = new ArrayList<>();
         try {
@@ -94,5 +179,6 @@ public class Day17 {
         }
         Day17 day17 = new Day17();
         day17.part1(input);
+        day17.part2(input);
     }
 }
